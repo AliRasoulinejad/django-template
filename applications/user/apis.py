e050fd58350c7ff008cf6c23eace8259aea38a72
ali.rasoulinejad <ali.rasoulinejad75@gmail.com>
2023-06-23 20:37:08 +0330

    add logger and debug codes

--- applications/user/apis.py
+++ applications/user/apis.py
@@ -1,3 +1,5 @@
+import logging
+
 from drf_spectacular.utils import extend_schema
 from rest_framework import serializers, status
 from rest_framework.response import Response
@@ -7,17 +9,19 @@ from result import Err
 from applications.user.models import User
 from applications.user.services.commands import user_register
 
+logger = logging.getLogger(__name__)
+
 
 class UserRegisterApi(APIView):
     class InputRegisterSerializer(serializers.Serializer):
         first_name = serializers.CharField(required=True)
         last_name = serializers.CharField(required=True)
         username = serializers.CharField(required=True)
-        password = serializers.EmailField(required=True)
-        password2 = serializers.EmailField(required=True)
+        password = serializers.CharField(required=True)
+        password2 = serializers.CharField(required=True)
 
         def validate_username(self, username):
-            if User.objects.filter(username=username).exists():
+            if User.objects.filter(username__exact=username).exists():
                 raise serializers.ValidationError("username Already Taken")
             return username
 
@@ -29,7 +33,6 @@ class UserRegisterApi(APIView):
                 raise serializers.ValidationError("password2 is not equal to password")
             return attrs
 
-
     class OutputRegisterSerializer(serializers.ModelSerializer):
         class Meta:
             model = User
@@ -39,12 +42,17 @@ class UserRegisterApi(APIView):
     def post(self, request):
         serializer = self.InputRegisterSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = user_register(**serializer.validated_data)
+        data = serializer.validated_data
+        user = user_register(
+            username=data["username"], password=data["password"], first_name=data["first_name"],
+            last_name=data["last_name"]
+        )
         if isinstance(user, Err):
+            logger.error(user.err())
+
             return Response(
                 f"Database Error {user.err()}",
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        return Response(self.OutputRegisterSerializer(user, context={"request": request}).data)
-
+        return Response(self.OutputRegisterSerializer(user.value, context={"request": request}).data)
