e90f80cffd833d61b039fda1e9b876973255a780
ali.rasoulinejad <ali.rasoulinejad75@gmail.com>
2023-06-25 23:52:26 +0330

    fix bugs

--- applications/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-
-urlpatterns = [
-    path("auth/", include(("applications.authentication.urls", "auth"))),
-    path("users/", include(("applications.user.urls", "user"))),
-]
--- config/django/production.py
+++
@@ -1,9 +0,0 @@
-from .base import *  # noqa
-
-DEBUG = False
-
-SECRET_KEY = env('SECRET_KEY')
-
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
-
-from config.settings.sentry import *  # noqa
